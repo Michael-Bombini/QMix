9605f0c228303fedd778e223e7fe06cf387f5021
Michael Bombini <bombini.michael@gmail.com>
2023-02-11 20:56:36 +0100

    Home adjusted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,42 @@
-import './Home.css'
+import { useEffect, useState } from "react";
+import CategoryLink from "../components/quiz/CategoryLink";
+import categories from "../data/categories";
+import { Question } from "../interfaces/Question";
+
+import "./Home.css";
+
 function Home() {
-  return <div className='home'>ciao a tutti</div>
+  const [questions, setQuestions] = useState<Question[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const resp = await fetch(
+        "https://opentdb.com/api.php?amount=10&category=31"
+      );
+      const data = await resp.json();
+      setQuestions(data.results);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>QMix</h1>
+      <p>Choice a category to start play</p>
+      <div className="container">
+        <ul className="flex g-12 justify-center align-center">
+          {categories.map((category) => (
+            <CategoryLink
+              key={category.categoryId}
+              categoryColor={category.categoryColor}
+              categoryName={category.categoryName}
+              categoryId={category.categoryId}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
