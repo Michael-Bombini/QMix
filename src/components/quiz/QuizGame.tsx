2be9a6858e992abefc6e26bf056eea61d43d8f51
Michael Bombini <bombini.michael@gmail.com>
2023-02-13 20:28:06 +0100

    Started quiz logic

---
+++ src/components/quiz/QuizGame.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { UserChoices } from '../../interfaces/UserChoices'
+
+export default function QuizGame({username, difficulty, categoryId , categoryName}: UserChoices) {
+  return (
+    <div></div>
+  )
+}
---
+++ src/interfaces/UserChoices.ts
@@ -0,0 +1,6 @@
+export interface UserChoices{
+    username: string;
+    difficulty: string;
+    categoryName: string;
+    categoryId: number;
+}
