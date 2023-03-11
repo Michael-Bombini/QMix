09f8ed2f16908cdcab49c749eeaf846f7940af34
Michael Bombini <bombini.michael@gmail.com>
2023-03-11 14:06:24 +0100

    Removed library

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 import App from "./App";
 import "./index.css";
-
-disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
